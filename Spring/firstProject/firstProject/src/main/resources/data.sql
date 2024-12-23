ea747cba2d87c3c85571dee66e60bdc8947d4265
YangHyunWook <126859790+idusnr1117@users.noreply.github.com>
2024-12-23 17:29:56 +0900

    Delete Spring/firstProject/firstProject directory

--- Spring/firstProject/firstProject/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into article(id, title, content) values(1, '가가가가가', '1111');
-insert into article(id, title, content) values(2, '나나나나나', '2222');
-insert into article(id, title, content) values(3, '다다다다다', '3333');
